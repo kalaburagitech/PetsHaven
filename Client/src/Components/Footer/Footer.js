d380689ae9670c303da809e8afb372e73d77442a
Santoshchalageri18 <santoshchalageri1999@gmail.com>
2025-01-01 14:31:24 +0530

    initial commit

---
+++ Client/src/Components/Footer/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "./images/logo.png";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div>
+        <Link className="logo-container" to="/">
+          <img className="navbar-logo" src={logo} alt="PetsHaven Logo" />
+          <p>{props.title}</p>
+        </Link>
+      </div>
+      <div className="below-footer">
+        <p>
+          You can reach me at{" "}
+          <a className="mail-links" href="mailto:kalaburagitech@gmail.com">
+          kalaburagitech@gmail.com
+          </a>
+        </p>
+        <p>
+          <a
+            className="contact-links"
+            href="https://www.linkedin.com/in/kalaburagitech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-linkedin-square"></i> Linkedin
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://github.com/kalaburagitech"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-github"></i> GitHub
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://www.instagram.com/kalaburagitech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-instagram"></i> Instagram
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://api.whatsapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-whatsapp"></i> WhatsApp
+          </a>
+        </p>
+        <p>&copy; 2025 KalaburagiTech Innovative Software Solutions</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
